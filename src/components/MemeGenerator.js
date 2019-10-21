4347ad1203bdb99bc5e359ecd836d92f7986cfd4
bulidiriba <bulidiriba@gmail.com>
2019-10-21 12:51:38 +0300

    define handleChange for the input text

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -3,8 +3,22 @@ import React, {Component} from 'react'
 class MemeGenerator extends Component {
     constructor(){
         super()
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
+        this.handleChange = this.handleChange.bind(this)
+        
     }
 
+    handleChange(event){
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
     
     
     render(){
@@ -17,14 +31,14 @@ class MemeGenerator extends Component {
                         name='topText'
                         value={this.state.topText}
                         placeholder='top text'
-                        onChange={}
-                    />
+                        onChange={this.handleChange}
+                    /> 
                     <input 
                         type='text'
                         name='bottomText'
                         value={this.state.bottomText}
                         placeholder='bottom text'
-                        onChange={}
+                        onChange={this.handleChange}
                     />
                     <button>Gen</button>
                 </form>
