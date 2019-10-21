7e7bdb42f4e7547c603b76207afe4aa4dfa523f6
bulidiriba <bulidiriba@gmail.com>
2019-10-21 12:49:56 +0300

    create a form that takes text

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -10,6 +10,25 @@ class MemeGenerator extends Component {
     render(){
         return(
             <div>
+                <form className='meme-form' onSubmit={this.handleSubmit}>
+                    
+                    <input
+                        type="text"
+                        name='topText'
+                        value={this.state.topText}
+                        placeholder='top text'
+                        onChange={}
+                    />
+                    <input 
+                        type='text'
+                        name='bottomText'
+                        value={this.state.bottomText}
+                        placeholder='bottom text'
+                        onChange={}
+                    />
+                    <button>Gen</button>
+                </form>
+
                 
             </div>
         )
