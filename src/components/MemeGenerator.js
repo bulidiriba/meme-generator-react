8371621b884f5a61f14480ef9f51e024722f37da
bulidiriba <bulidiriba@gmail.com>
2019-10-21 12:58:39 +0300

    fetch meme images from api

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -13,14 +13,22 @@ class MemeGenerator extends Component {
         
     }
 
+    componentDidMount(){
+        fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json())
+        .then(response => { 
+            const {memes} = response.data
+            this.setState({allMemeImgs: memes })
+        })
+    }
+
     handleChange(event){
         const {name, value} = event.target
         this.setState({
             [name]: value
         })
     }
-    
-    
+
     render(){
         return(
             <div>
